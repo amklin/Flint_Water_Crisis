affd643c5fc35bd3a97673028c46c519e6776e56
Amanda Lin <amandaklin257@gmail.com>
2021-12-16 17:56:32 -0500

    Swiper functionality
    main functionality of swiper is now working (you can swipe back and forth) BUT aesthetics needs to be improved... will work on that later

--- js/timeline.js
+++ js/timeline.js
@@ -1,18 +1,33 @@
-var timelineSwiper = new Swiper ('.timeline .swiper-container', {
-    direction: 'vertical',
-    loop: false,
-    speed: 1600,
-    pagination: '.swiper-pagination',
-    paginationBulletRender: function (swiper, index, className) {
-      var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
-      return '<span class="' + className + '">' + year + '</span>';
-    },
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    breakpoints: {
-      768: {
-        direction: 'horizontal',
-      }
+var timelineSwiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  
+  paginationClickable: true,
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
     }
-  });
+  }
+});
